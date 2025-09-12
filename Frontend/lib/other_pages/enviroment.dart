4e32180a1652a9cfde0de2f1f55965c694f1b936
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-09-12 08:40:36 +0530

    oho-32(Adding Transaction Working)

--- Frontend/lib/other_pages/enviroment.dart
+++ Frontend/lib/other_pages/enviroment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const String baseUrl = 'http://192.168.1.10:3000';
+  static const String baseUrl = 'http://10.231.7.219:3000';
   static const String serverClientId =
       "278433619849-b2hr3i6j0agk1mn8ctffq7pikhjdip2k.apps.googleusercontent.com";
 }
