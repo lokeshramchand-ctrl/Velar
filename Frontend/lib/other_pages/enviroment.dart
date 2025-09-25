498b43fe5cb2f74974a4226b67ef2f4bf516c51e
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-09-25 11:44:33 +0530

    OHO-54(Deploy Testing)

--- Frontend/lib/other_pages/enviroment.dart
+++ Frontend/lib/other_pages/enviroment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const String baseUrl = 'https://velar-back.azurewebsites.net:3000';
+  static const String baseUrl = 'https://velar-backend.azurewebsites.net:3000';
   static const String serverClientId =
       "278433619849-b2hr3i6j0agk1mn8ctffq7pikhjdip2k.apps.googleusercontent.com";
 }
