7abd66c5473de04751aa298e6c30aaf70a2dc5bb
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-09-25 09:38:34 +0530

    OHO-53(Deploy Testing)

--- Frontend/lib/other_pages/enviroment.dart
+++ Frontend/lib/other_pages/enviroment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const String baseUrl = 'http://10.231.40.44:3000';
+  static const String baseUrl = 'http://10.231.48.29:3000';
   static const String serverClientId =
       "278433619849-b2hr3i6j0agk1mn8ctffq7pikhjdip2k.apps.googleusercontent.com";
 }
