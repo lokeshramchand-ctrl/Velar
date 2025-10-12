62b58983bfb9b2c3fb18354ef5e1cd371bdec6eb
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-10-12 17:55:42 +0530

    Working on almost completed authe

--- Frontend/lib/other_pages/enviroment.dart
+++ Frontend/lib/other_pages/enviroment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const String baseUrl = 'http://192.168.1.7:3000';
+  static const String baseUrl = 'http://192.168.1.10:3000';
   static const String serverClientId =
       "278433619849-b2hr3i6j0agk1mn8ctffq7pikhjdip2k.apps.googleusercontent.com";
 }
