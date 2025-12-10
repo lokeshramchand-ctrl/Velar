998e3efa2a915e602f9d8ddced16db312864ae78
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-12-11 00:00:07 +0530

    Update base URL in Environment class

--- Frontend/lib/other_pages/enviroment.dart
+++ Frontend/lib/other_pages/enviroment.dart
@@ -1,6 +1,6 @@
 class Environment {
   static const String baseUrl =
-      'http://ec2-13-60-188-145.eu-north-1.compute.amazonaws.com:3000';
+      'http://172.16.0.2:3000';
   static const String serverClientId =
       "278433619849-b2hr3i6j0agk1mn8ctffq7pikhjdip2k.apps.googleusercontent.com";
 }
